2f1c94107c1743ab4131330bf1b177a87587d1bb
Lyudmyla <kaluzhynova@gmail.com>
2017-02-28 09:29:07 +0200

    t1 views

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Person
 
 
 # Create your views here.
 def detail(request):
-    return render(request, "hello/person.html")
+    person = Person.objects.first()
+    return render(request, "hello/person.html", {"person": person, })
