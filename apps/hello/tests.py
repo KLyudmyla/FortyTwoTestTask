87dbf61dedb8237ea7b344254787db8457214a7e
Lyudmyla <kaluzhynova@gmail.com>
2017-03-01 20:44:47 +0200

    t1 django template with hard-coded data fix2

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -3,7 +3,6 @@ from. models import Person
 
 
 # Create your tests here.
-
 class PersonDetailTest(TestCase):
     class Meta:
         model = Person
@@ -11,37 +10,33 @@ class PersonDetailTest(TestCase):
     def test_pages_person_detail_200(self):
         client = Client()
         person1 = Person.objects.create(
-		               name = "Lyuda",
-                               surname = "Kaluzhynova",
-                               bio = "biography",
-                               date_birth = '1980-06-14',
-		               email = 'vlyuda@mail.ru',
-                               jabber = 'lyudmyla@22cc.co', 
-                               skype = 'kaluzhynoval',
+                               name="Lyuda",
+                               surname="Kaluzhynova",
+                               bio="biography",
+                               date_birth='1980-06-14',
+		               email='vlyuda@mail.ru',
+                               jabber='lyudmyla@22cc.co',
+                               skype='kaluzhynoval',
 			       )
-        response = client.get('/')
+        response=client.get('/')
         self.assertEqual(response.status_code, 200)
 
 
     def test_person_detail(self):
         client = Client()
         person1 = Person.objects.create(
-		               name = "Lyuda",
-                               surname = "Kaluzhynova",
-                               bio = "biography",
-                               date_birth = '1980-06-14',
-		               email = 'vlyuda@mail.ru',
-                               jabber = 'lyudmyla@22cc.co', 
-                               skype = 'kaluzhynoval', 
+                               name="Lyuda",
+                               surname="Kaluzhynova",
+                               bio="biography",
+                               date_birth='1980-06-14',
+		               email='vlyuda@mail.ru',
+                               jabber='lyudmyla@22cc.co',
+                               skype='kaluzhynoval', 
                                   )
         response = client.get('/')
         self.assertContains(response, 'Lyudmyla')
         self.assertContains(response, "Kaluzhynova")
         self.assertContains(response, "biography")
- #       self.assertContains(response, "1980-06-14")
-  #      self.assertContains(response, "vlyuda@mail.ru")
-  #      self.assertContains(response, "lyudmyla@22cc.co")
-  #      self.assertContains(response, "kaluzhynoval")
 
 
     def test_pages_person_empty(self):
@@ -51,8 +46,6 @@ class PersonDetailTest(TestCase):
         self.assertContains(response, "42 Coffee Cups Test Assignment")
         self.assertContains(response, "Name")
         self.assertContains(response, "Last name")
-
-
 
 
 class SomeTests(TestCase):
